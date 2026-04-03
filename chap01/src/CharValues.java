209b0ae3c6c1445e05083626c02e9bf8fa891a8a
ToffeeSta <wjdgnsrh@gmail.com>
2026-04-03 19:34:07 +0100

    test

---
+++ chap01/src/CharValues.java
@@ -0,0 +1,11 @@
+public class CharValues {
+    public static void main(String[] args) {
+        char munja1 = 'A'; // 65 (ascii 코드값 넣는 것과 같음)
+        char munja2 = 98-32;
+        char munja3 = 99-32;
+        char munja4 = 100-32;
+        char munja5 = 101-32;
+
+        System.out.println(munja1+""+munja2+""+munja3+""+munja4+""+munja5);
+    }
+}
---
+++ chap01/src/RealNumberValues.java
@@ -0,0 +1,14 @@
+public class RealNumberValues {
+
+    public static void main(String[] args) {
+        float minFloat = Float.MIN_VALUE;
+        float maxFloat = Float.MAX_VALUE;
+        double minDouble = Double.MIN_VALUE;
+        double maxDouble = Double.MAX_VALUE;
+
+        System.out.println("float의 최소 양수 값 " + minFloat);
+        System.out.println("float의 최대 양수 값 " + maxFloat);
+        System.out.println("double의 최소 양수 값" + minDouble);
+        System.out.println("double의 최대 양수 값" + maxDouble);
+    }
+}
